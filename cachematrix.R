d4a8a27f8d259c8931b218e894778e3699c423ea
Elimarini <Elimarini@users.noreply.github.com>
2014-10-23 17:01:36 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## Matrix inversion is usually a costly computation and their may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly. 
+## The assignment is to write a pair of functions that cache the inverse of a 
+## matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+## The function makeCacheMatrix function creates a special "matrix" object that 
+## can cache its inverse. The function contains: 
 
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse of the matrix
+## 4.get the value of the inverse of the matrix
+
+
+makeCacheMatrix<-function(x=matrix()) {
+  i <- NULL
+## Set matrix  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+## Get matrix
+  get <- function() x
+## Set and get the inverse of the matrix  
+  setinverse <- function(inverse) 
+    i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then the cachesolve should retrieve the inverse 
+## from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Compute the inverse of 'x'  
+  i <- x$getinverse()
+## If the inverse has already been calculated - return it  
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...) 
+  x$setinverse(i)
+  i
 }
